8b4f99d64d2442c7a7e1a2d8a702911293a8db2c
ZeeshanAhmed <zeeshanamed98@gmail.com>
2024-11-28 21:50:26 +0500

    added coming soon code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import ProcomVideo from './assets/Procom_gif.gif'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='screen-bg'>
+    <img className="bg-video" src={ProcomVideo} alt='Procom25 Coming Soon'/>
     </div>
   );
 }
