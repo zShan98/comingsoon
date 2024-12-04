12edf3a70fe04854deef72e295e24a8b697ebd7a
ZeeshanAhmed <zeeshanamed98@gmail.com>
2024-12-04 16:06:06 +0500

    width resize adjustment and video change for diffrent sizes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React, { useState, useEffect } from "react";
 import './App.css';
-import ProcomVideo from './assets/Procom_gif.gif'
+import ProcomVideoLaptop from './Procom25_laptop.gif'
+import ProcomVideoMobile from './Procom_gif_mobile.gif'
+
 function App() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <div className='screen-bg'>
-    <img className="bg-video" src={ProcomVideo} alt='Procom25 Coming Soon'/>
+    <img className="bg-video" src={width > 450? ProcomVideoLaptop : ProcomVideoMobile}  alt='Procom25 Coming Soon'/>
     </div>
   );
 }
